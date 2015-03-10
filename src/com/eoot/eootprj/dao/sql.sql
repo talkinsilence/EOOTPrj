26c39827d81b314eb4de5af6e6f65e8a090e02bb
nami jang <talkinsilence21@gmail.com>
2015-03-10 15:32:58 +0900

    nami : create Letter.java @com.eoot.eootprj.model

--- src/com/eoot/eootprj/dao/sql.sql
+++ src/com/eoot/eootprj/dao/sql.sql
@@ -5,3 +5,5 @@ delete from members;
 INSERT INTO MEMBERS(MID, PWD, NAME, BIRTHDAY, REGDATE, FAMCODE) VALUES('ddddd', 'ddfd', 'dfasdf', '19910101', GETDATE(), 'tete');
 
 select * from members where Mid = 'ddd';
+
+select * from letters;
